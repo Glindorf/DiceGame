7e69de66586ea2276f2bac21bfe07efc8898fa69
Madeleine Glindorf <mglindorf@gmail.com>
2020-03-30 13:24:31 +0200

    Initial commit

---
+++ InsertCodeHere.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace DiceGame
+{
+    public class InsertCodeHere
+    {
+        public void MyCode()
+        {
+            // The FIRST line of code should be BELOW this line
+
+            DiceCup myDie = new DiceCup();
+
+            System.Console.WriteLine($"Du har slået: {myDie.TotalValue}");
+            // The LAST line of code should be ABOVE this line
+        }
+    }
+}
